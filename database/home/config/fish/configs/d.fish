35270861affa8a219e337836139615bcca86782b
LiAlH4 <lialh4qwq@outlook.com>
2025-09-28 23:53:41 +0800

    support D: on laptop

--- database/home/config/fish/configs/d.fish
+++ database/home/config/fish/configs/d.fish
@@ -1,1 +1,7 @@
-set -gx LOCAL_DRIVE_D "/media/hdd/lialh4"
+set pcD "/media/hdd/lialh4"
+set laptopD "/mnt/data/lialh4"
+if [ -d $pcD ]
+    set -gx LOCAL_DRIVE_D $pcD
+else
+    set -gx LOCAL_DRIVE_D $laptopD
+end
